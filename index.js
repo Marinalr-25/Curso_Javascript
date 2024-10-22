2f75758c8e54b3a942e074459249d346aa82096a
Marina Lopes <marina.roberto2020@gmail.com>
2024-10-22 15:20:37 -0300

    Estou adicionando a funcionalidade de Log Colorido

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+import { turboLooger } from './turboLogger.js';
+
 console.log('Estou usando o github');
+turboLooger('Estou aprendendo o fluxo do git', 'pink');
---
+++ turboLogger.js
@@ -0,0 +1,4 @@
+export const turboLooger = (message, textColor = 'Blue') => {
+  const textStyle = `color: ${textColor} `;
+  console.log(`%c${message}`, textStyle);
+};
