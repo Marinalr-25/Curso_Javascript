0ddd932a087f0e2c1de9813c15fb7d57c5f6f09e
Marina Lopes <marina.roberto2020@gmail.com>
2024-10-22 16:00:55 -0300

    atualizada as mensagens

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import { turboLooger } from './turboLogger.js';
 
 console.log('Estou usando o github');
 turboLooger('Estou aprendendo o fluxo do git', 'pink');
+turboLooger('Eu consegui criar uma nova mensagem', 'darkgreen');
